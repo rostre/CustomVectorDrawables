3d8a92f4c4e69d062891d66afc8f6053fc9228d2
Tudor <trenchea_tudor@yahoo.com>
2020-12-23 23:51:42 +0200

    Initial commit

---
+++ app/src/main/java/ro/twodoors/customvectordrawables/MainActivity.kt
@@ -0,0 +1,11 @@
+package ro.twodoors.customvectordrawables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
